26d531d5c6e013c1580b140da8e7a5c85dc66fc4
udayz84 <udayznanam@gmail.com>
2025-05-25 14:55:11 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  eslint: {
+    // This allows production builds to successfully complete
+    // even if your project has ESLint errors
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
